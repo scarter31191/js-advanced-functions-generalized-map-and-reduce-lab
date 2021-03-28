fec44defdcd0688e5d1be077ccabf0199937ea53
scarter31191 <scarter31191@gmaile.com>
2021-03-27 21:31:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your functions here
+function map(array, func) {
+    const x = []
+
+    for(const i in array) {
+        x.push(func(array[i]))
+    }
+    return x
+};
+
+function reduce(array, func, starting) {
+    let i = (!!starting) ? starting : array[0]
+    let x = (!!starting) ? 0 : 1
+
+    for (; x < array.length; x++) {
+        i = func(array[x], i)
+    }
+    return i
+}
